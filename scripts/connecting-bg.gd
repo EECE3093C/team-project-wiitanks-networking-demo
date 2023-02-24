9ad89ab210348690b21a1c04a352e6be03a6413d
Ryan Williams <ryan.williams926@icloud.com>
2023-02-24 14:12:24 -0500

    connecting-bg handles its own hiding

--- scripts/connecting-bg.gd
+++ scripts/connecting-bg.gd
@@ -4,6 +4,9 @@ const HOST_CONNECT_TEXT = "Waiting for Players..."
 const CLIENT_CONNECT_TEXT = "Connecting to Server..."
 onready var loading_text = get_node("connecting-text")
 
+func _ready():
+	hide()
+
 # wire this to the host and join buttons. set the text of the loading
 # screen to match the situation.
 func set_connect_type(asHost):
